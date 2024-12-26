da421138d939ca00e053fdcbab420771b81b29cf
davig27 <davigrammbauer@gmail.com>
2024-12-26 13:34:47 -0300

    Implemented relative offset

--- ObjectMesh2D.js
+++ ObjectMesh2D.js
@@ -214,6 +214,7 @@ class Measure {
 class Style {
 	_parentElement;
 	style = {
+		_relativeOffsetX: 0, _relativeOffsetY: 0,
 		_measure: new Measure(1), _marginMeasureX: 0, _marginMeasureY: 0, _marginX: 0, _marginY: 0,
 		_fill: true, _hidden: false, _bgColor: "transparent", _lineWidth: 1, _zIndex: 1
 	};
@@ -243,13 +244,13 @@ class Style {
 
         switch (alignDirection) {
 			case "left":
-				element.move(0, 0, (cX-element.x), 0);
+				element.relativeOffsetX = cX;
 				break;
             case "center":		
-				element.move(0, 0, ((cWidth - element.width)/2) - (element.x - cX), 0);				
+				element.relativeOffsetX = cX + ((cWidth - element.width)/2);
                 break;
 			case "right":
-				element.move(0, 0, (cWidth - element.width) + (cX - element.x), 0);
+				element.relativeOffsetX = cX + cWidth - element.width;
 				break;
         }
     }
@@ -284,17 +285,21 @@ class Style {
 		
         switch (alignDirection) {
 			case "top":
-				element.move(0, 0, 0, (cY-element.y) + biasY);
+				element.relativeOffsetY = cY + biasY;
 				break;
 			case "center":
-				element.move(0, 0, 0, ((cHeight - element.height)/2) - (element.y - cY) + biasY);
+				element.relativeOffsetY = cY + ((cHeight - element.height)/2) + biasY;
 				break;
 			case "bottom":
-				element.move(0, 0, 0, (cHeight - element.height) + (cY - element.y) + biasY);
+				element.relativeOffsetY = cY + cHeight - element.height + biasY;
 				break;
         }
     }
 	
+	/** Relative offset X to it's parent. */
+	get relativeOffsetX() { return this.style._relativeOffsetX }
+	/** Relative offset Y to it's parent. */
+	get relativeOffsetY() { return this.style._relativeOffsetY }
 	/** Custom measure in pixels. */
 	get measure() { return this.style._measure }
 	/** Margin X in measure. */
@@ -306,9 +311,9 @@ class Style {
 	/** Margin Y in pixel. */ 
 	get marginY() { return this.style._marginY }
 	/** Coordinate X in pixels */
-	get x() { return (this.marginMeasureX * this.measure) + this.marginX }
+	get x() { return this.relativeOffsetX + (this.marginMeasureX * this.measure) + this.marginX }
 	/** Coordinate Y in pixels */
-	get y() { return (this.marginMeasureY * this.measure) + this.marginY }
+	get y() { return this.relativeOffsetY + (this.marginMeasureY * this.measure) + this.marginY }
 
 	get parentElement() { return this._parentElement }
 	get fill() { return this.style._fill }
@@ -317,6 +322,8 @@ class Style {
 	get lineWidth() { return this.style._lineWidth }
 	get zIndex() { return this.style._zIndex ?? 1 }
 
+	set relativeOffsetX(position) { this.style._relativeOffsetX = typeof(position) === "number" ? position : this.style._relativeOffsetX }
+	set relativeOffsetY(position) { this.style._relativeOffsetY = typeof(position) === "number" ? position : this.style._relativeOffsetY }
 	set measure(measure) { this.style._measure = measure instanceof Measure || typeof(measure) === "number" ? measure : this.style._measure }
 	set marginMeasureX(measurePosition) { this.style._marginMeasureX = typeof(measurePosition) === "number" ? measurePosition : this.style._marginMeasureX }
 	set marginMeasureY(measurePosition) { this.style._marginMeasureY = typeof(measurePosition) === "number" ? measurePosition : this.style._marginMeasureY }
@@ -585,7 +592,6 @@ class ComplexObject extends Rectangle {
 	get showDisplayArea() { return this.style._showDisplayArea }
 	set showDisplayArea(value) { this.style._showDisplayArea = value !== false ? true : false }
 
-
 	get alignX() { return this.style._alignX }
 	get alignY() { return this.style._alignY }
 
@@ -633,7 +639,8 @@ class ComplexObject extends Rectangle {
 		if (reverse) shapes.reverse();
 		for (const shape of shapes) {
 			shape.parentElement = this;
-			shape.move(0, 0, this.alignX ? 0 : this.x, this.alignY ? 0 : this.y);
+			shape.relativeOffsetX = this.x;
+			shape.relativeOffsetY = this.y;
 			Style.alignX(this.style._alignX, shape, this);
 			Style.alignY(this.style._alignY, shape, this);
 			if (unshift) {
@@ -650,6 +657,24 @@ class ComplexObject extends Rectangle {
 			Style.alignX(this.style._alignX, shape, this);
 			Style.alignY(this.style._alignY, shape, this);
 		}
+	}
+
+	get relativeOffsetX() { return super.relativeOffsetX }
+	get relativeOffsetY() { return super.relativeOffsetY }
+
+	set relativeOffsetX(position) {
+		const deltaOffsetX = position - this.relativeOffsetX;
+		for (const shape of this.shapes) {
+			shape.relativeOffsetX += deltaOffsetX;
+		}
+		super.relativeOffsetX = position;
+	}
+	set relativeOffsetY(position) {
+		const deltaOffsetY = position - this.relativeOffsetY;
+		for (const shape of this.shapes) {
+			shape.relativeOffsetY += deltaOffsetY;
+		}
+		super.relativeOffsetY = position;
 	}
 }
 
@@ -766,8 +791,9 @@ class CText extends Shape {
 	}
 
 	/** Recalculate values when font size change. 
-	 * @param {number} currentFontSize 
-	 * @param {number} newFontSize 
+	 * @private
+	 * @param {number} currentFontSize
+	 * @param {number} newFontSize
 	*/
 	yCorrection(currentFontSize, newFontSize) {
 		const currentTotalLines = this.style._width ? Math.ceil((this.text.length*currentFontSize) / this.width) : 1;
