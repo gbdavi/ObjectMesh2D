3efc9d4db2911d1b063bcc350fc65c7baa39b413
davig27 <davigrammbauer@gmail.com>
2024-12-31 08:47:40 -0300

    Added RoutineManager

--- Engine.js
+++ Engine.js
@@ -1,12 +1,12 @@
 
-
+/** Manage state events. */
 class StateManager {
     /** @private */
     _states = {};
 
     /** Create state. 
      * @param {string} state State name.
-     * @param {Function} setStateEvent Event function (oldValue, newValue) when state value is setted5.
+     * @param {Function} setStateEvent Event function (oldValue, newValue) when state value is setted.
      * @param {*} initialValue Initial value.
     */
     createState(state, setStateEvent, initialValue) {
@@ -17,6 +17,7 @@ class StateManager {
             setStateEvent,
             value: initialValue ?? undefined
         }
+        return true;
     }
     
     /** Get state value. 
@@ -28,7 +29,7 @@ class StateManager {
         return this._states[state].value;
     }
 
-    /** Set state value 
+    /** Set state value.
      * @param {string} state State name.
      * @param {*} value New value.
     */
@@ -43,4 +44,77 @@ class StateManager {
         }
         return oldValue;
     }
+}
+
+/** Manage routine executions. */
+class RoutineManager {
+    /** @private */
+    _routines = {};
+    /** @private */
+    _intervalExecutions = {};
+
+    /**
+     * Create new routine. 
+     * @param {string} name 
+     * @param {Function} routineFunction
+     * @param {number} interval
+     * @param {number} executionOrder
+     * @param {boolean} isEnabled
+     */
+    createRoutine(name, routineFunction, interval, executionOrder, isEnabled) {
+        if (this._routines[name])
+            return false;
+
+        const routine = {
+            routineFunction,
+            interval,
+            executionOrder: executionOrder ?? 1,
+            isEnabled: isEnabled ?? true
+        }
+        this._routines[name] = routine;
+        if (!this._intervalExecutions[interval])
+            this._intervalExecutions[interval] = {interval, routines: []};
+        this._intervalExecutions[interval].routines.push(routine);
+        this._intervalExecutions[interval].routines.sort((a, b) => a.executionOrder - b.executionOrder);
+        return true;
+    }
+
+    /**
+     * Enable/disable routine.
+     * @param {boolean} state 
+     */
+    setRoutineState(routine, state) {
+        if (!this._routines[routine])
+            return false;
+
+        this._routines[routine].isEnabled = state;
+        return true;
+    }
+
+    /** Start all routines. */
+    startRoutines() {
+        for (const execution of Object.values(this._intervalExecutions)) {
+            if (execution.id)
+                continue;
+
+            execution.id = setInterval(() => {
+                for (const routine of execution.routines) {
+                    if (routine.isEnabled) {
+                        routine.routineFunction();
+                    }
+                }
+            }, execution.interval);
+        }
+    }
+
+    /** Stop all routines. */
+    stopRoutines() {
+        for (const execution of Object.values(this._intervalExecutions)) {
+            if (execution.id) {
+                clearInterval(execution.id);
+                execution.id = undefined;
+            }
+        }
+    }
+
 }
